5f9adfeedf71cb82b38d2f1273faf9ca0c1929e4
TarsisiusRisto <risto.tarsisius@gmail.com>
2025-01-26 00:21:39 +0700

    add gallery

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { useEffect, useState } from "react";
 import About from "./components/About";
 import Gallery from "./components/Gallery";
 import Header from "./components/Header";
@@ -7,29 +6,6 @@ import Navbar from "./components/navigation/Navbar";
 import galleryData from "./components/models/galleryData";
 
 export default function Home() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  useEffect(() => {
-    if (
-      localStorage.theme === "dark" ||
-      (!("theme" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
-    ) {
-      setIsDarkMode(true);
-    } else {
-      setIsDarkMode(false);
-    }
-  });
-
-  useEffect(() => {
-    if (isDarkMode) {
-      document.documentElement.classList.add("dark");
-      localStorage.theme = "dark";
-    } else {
-      document.documentElement.classList.remove("dark");
-      localStorage.theme = "";
-    }
-  }, [isDarkMode]);
   return (
     <div className="overflow-y-auto scroll-smooth">
       <div>

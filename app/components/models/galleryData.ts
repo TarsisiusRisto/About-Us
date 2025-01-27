9ef4cc28de2c3fec9ee20e43d6cf3781068d0074
TarsisiusRisto <risto.tarsisius@gmail.com>
2025-01-27 16:50:13 +0700

    update photo gallery

--- app/components/models/galleryData.ts
+++ app/components/models/galleryData.ts
@@ -11,8 +11,9 @@ const galleryData: GalleryItem[] = [
   { src: "/assets/Gallery/u1.JPG", alt: "Photo 1", type: "image" },
   { src: "/assets/Gallery/u2.JPG", alt: "Photo 3", type: "image" },
   { src: "/assets/Gallery/u4.JPG", alt: "Photo 4", type: "image" },
-  { src: "/assets/Gallery/u6.JPG", alt: "Photo 5", type: "image" },
-  { src: "/assets/Gallery/u7.JPG", alt: "Photo 6", type: "image" },
+  { src: "/assets/Gallery/u5.jpg", alt: "Photo 5", type: "image" },
+  { src: "/assets/Gallery/u6.jpg", alt: "Photo 6", type: "image" },
+  { src: "/assets/Gallery/u7.jpg", alt: "Photo 7", type: "image" },
 ];
 
 export default galleryData;
